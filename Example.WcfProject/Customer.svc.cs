58413b2d4d2e0f8ec1d43d84099f6f0906f8560f
NostalgicCoder <spb.work@outlook.com>
2024-04-19 14:51:02 +0100

    Add project files.

---
+++ Example.WcfProject/Customer.svc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ServiceModel.Activation;
+
+namespace Example.WcfProject
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Customer : ICustomer
+    {
+        public CustomerResponse AddCustomer(CustomerRequest request)
+        {
+            CustomerResponse response = new CustomerResponse();
+
+            try
+            {
+                response.Success = true;
+            }
+            catch(Exception ex)
+            {
+                response.Success = false;
+                response.ErrorMsg = ex.ToString();
+            }
+
+            return response;
+        }
+
+        public ReturnYourNameResponse ReturnYourName(CustomerRequest request)
+        {
+            ReturnYourNameResponse response = new ReturnYourNameResponse();
+
+            try
+            {
+                if (!string.IsNullOrEmpty(request.CustomerDetails.Forename) && !string.IsNullOrEmpty(request.CustomerDetails.Surname))
+                {
+                    response.YourName = string.Format("{0} {1}", request.CustomerDetails.Forename, request.CustomerDetails.Surname);
+                    response.Success = true;
+                }
+                else
+                {
+                    response.Success = false;
+                    response.ErrorMsg = "Missing forename and surname";
+                }
+            }
+            catch(Exception ex)
+            {
+                response.Success = false;
+                response.ErrorMsg = ex.ToString();
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Example.WcfProject/ICustomer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace Example.WcfProject
+{
+    [DataContract]
+    public class ValidationResponse
+    {
+        [DataMember(IsRequired = true)]
+        public bool Success;
+
+        [DataMember]
+        public string ErrorMsg;
+    }
+
+    [DataContract]
+    public class AddressDetails
+    {
+        [DataMember(IsRequired = true)]
+        public string NameNumber;
+
+        [DataMember(IsRequired = true)]
+        public string StreetLine1;
+
+        [DataMember]
+        public string StreetLine2;
+
+        [DataMember(IsRequired = true)]
+        public string Town;
+
+        [DataMember]
+        public string County;
+
+        [DataMember(IsRequired = true)]
+        public string PostCode;
+
+        [DataMember]
+        public string Country;
+    }
+
+    [DataContract]
+    public class CustomerDetails
+    {
+        [DataMember(IsRequired = true)]
+        public string Forename;
+
+        [DataMember(IsRequired = true)]
+        public string Surname;
+
+        [DataMember(IsRequired = true)]
+        public DateTime DOB;
+    }
+
+    [DataContract]
+    public class CustomerRequest
+    {
+        [DataMember(IsRequired = true)]
+        public CustomerDetails CustomerDetails;
+
+        [DataMember(IsRequired = true)]
+        public AddressDetails AddressDetails;
+    }
+
+    [DataContract]
+    public class CustomerResponse : ValidationResponse
+    {
+    }
+
+    [DataContract]
+    public class ReturnYourNameResponse : ValidationResponse
+    {
+        [DataMember(IsRequired = true)]
+        public string YourName;
+    }
+
+    [ServiceContract]
+    public interface ICustomer
+    {
+        [OperationContract]
+        CustomerResponse AddCustomer(CustomerRequest request);
+
+        [OperationContract]
+        ReturnYourNameResponse ReturnYourName(CustomerRequest request);
+    }
+}
